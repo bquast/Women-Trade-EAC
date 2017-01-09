4db5fd0427c5b2c21f92001f686606a8f241f4eb
Bastiaan Quast <Bastiaan.Quast@unctad.org>
2017-01-09 15:13:39 +0100

    fix importing and saving, add exploring Burundi enterprise

--- R/import-enterprise-Burundi.R
+++ R/import-enterprise-Burundi.R
@@ -7,16 +7,17 @@
 library(haven)
 
 # import Burundi enterprise
-Burundi_2006 <- read_dta("~/Women-Trade-EAC/data/Enterprise/Burundi/Burundi-2006-Employees-full data-1.dta")
-Burundi_2014 <- read_dta("~/Women-Trade-EAC/data/Enterprise/Burundi/Burundi-2014-full data.dta")
-Burundi_2006_Micro <- read_dta("~/Women-Trade-EAC/data/Enterprise/Burundi/Burundi-2006-Micro-full data-1.dta")
+Burundi_2006 <- read_dta(file = "data/Enterprise/Burundi/Burundi-2006-Employees-full data-1.dta")
+Burundi_2014 <- read_dta(file = "data/Enterprise/Burundi/Burundi-2014-full data.dta")
+Burundi_2006_Micro <- read_dta(file = "data/Enterprise/Burundi/Burundi-2006-Micro-full data-1.dta")
 
 # save
-save.image("~/Women-Trade-EAC/data/Enterprise/Burundi/Burundi-Enterprise.R.RData")
+save.image(Burundi_2006, Burundi_2014, Burundi_2006_Micro, file = "data/Enterprise/Burundi/Burundi-Enterprise.RData")
 
 # load explore library
 library(explore)
 
 # explore data
-
-
+explore(Burundi_2006)
+explore(Burundi_2014)
+explore(Burundi_2006_Micro)
