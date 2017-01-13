9a9c8c47078c9071632eadf61323f99b74aa0f7d
Bastiaan Quast <Bastiaan.Quast@unctad.org>
2017-01-13 11:42:42 +0100

    fix saving error

--- R/Enterprise-Burundi-import.R
+++ R/Enterprise-Burundi-import.R
@@ -12,7 +12,10 @@ Burundi_2014 <- read_dta(file = "data/Enterprise/Burundi/Burundi-2014-full data.
 Burundi_2006_Micro <- read_dta(file = "data/Enterprise/Burundi/Burundi-2006-Micro-full data-1.dta")
 
 # save
-save.image(Burundi_2006, Burundi_2014, Burundi_2006_Micro, file = "data/Enterprise/Burundi/Burundi-Enterprise.RData")
+save(Burundi_2006,
+     Burundi_2014,
+     Burundi_2006_Micro,
+     file = "data/Enterprise/Burundi/Burundi-Enterprise.RData")
 
 # load explore library
 library(explore)
