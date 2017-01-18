2d746e73efc44cc5322e3812af67fbdc00faf7dd
Bastiaan Quast <Bastiaan.Quast@unctad.org>
2017-01-18 10:11:50 +0100

    move computing ratio to above saving

--- R/Enterprise-Kenya-import.R
+++ R/Enterprise-Kenya-import.R
@@ -12,6 +12,10 @@ Kenya_2013 <- read_dta(file = "data/Enterprise/Kenya/Kenya-2013-full data.dta")
 Kenya_2007_Micro <- read_dta(file = "data/Enterprise/Kenya/Kenya_2007_cleanmicro.dta")
 Kenya_2013_Micro <- read_dta(file = "data/Enterprise/Kenya/Kenya_Micro-2013-full data.dta")
 
+# compute ratios
+Kenya_2013$gender_ratio_nonprod <- Kenya_2013$l5b / Kenya_2013$l3b
+Kenya_2013$gender_ratio_prod <- with(Kenya_2013, l5a/(l4a + l4b))
+
 # save
 save(Kenya_2007,
      Kenya_2013,
@@ -23,8 +27,8 @@ save(Kenya_2007,
 library(explore)
 
 # explore data
-
 explore(Kenya_2007)
 explore(Kenya_2013)
 explore(Kenya_2007_Micro)
 explore(Kenya_2013_Micro)
+
