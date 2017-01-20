eb7b28f4e7c80f5bcf7c59e36209354495b95ea6
Bastiaan Quast <Bastiaan.Quast@unctad.org>
2017-01-20 10:42:23 +0100

    add basic model

--- R/Enterprise-Kenya-model.R
+++ R/Enterprise-Kenya-model.R
@@ -13,8 +13,8 @@ library(labelled)
 library(broom)
 
 # define models
-m1 <- formula() # ratio female prod workers
-m2 <- formula() # ratio female nonprod workers
+m1 <- formula(gender_ratio_prod ~ to_factor(e1) ) # ratio female prod workers
+m2 <- formula(gender_ratio_nonprod) # ratio female nonprod workers
 m3 <- formula() # (partially) owned by female
 m4 <- formula() # percentage owned by female
 
@@ -26,3 +26,5 @@ m1_r2 <- lm(m1, data = Kenya_2013)
 # view output
 summary(m1_r1)
 summary(m1_r2)
+
+tidy(m1_r2)
