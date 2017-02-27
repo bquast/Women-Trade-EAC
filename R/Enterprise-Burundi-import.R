055bab20b104c228ffd123072e0b5790bdc626b2
Bastiaan Quast <bquast@gmail.com>
2017-02-27 14:50:55 +0100

    remove manu since now the main 2006 dataset

--- R/Enterprise-Burundi-import.R
+++ R/Enterprise-Burundi-import.R
@@ -14,10 +14,6 @@ Burundi_2014 <- read_dta(file = "data/Enterprise/Burundi/Burundi-2014-full data.
 Burundi_2014$female_share_nonprod <- Burundi_2014$l5b / Burundi_2014$l3b
 Burundi_2014$female_share_prod <- with(Burundi_2014, l5a/(l4a + l4b))
 
-# computer ratios 2006
-Burundi_2006_manu$female_share_nonprod <- with(Burundi_2006_manu, j2b2 / j2a2)
-Burundi_2006_manu$female_share_prod <- with(Burundi_2006_manu, j2b1 / j2a1)
-
 # simplify industry
 Burundi_2014$a4c <- ifelse(Burundi_2014$a4b < 20, 'Agriculture', ifelse(Burundi_2014$a4b < 40, 'Manufacturing', 'Services') )
 
