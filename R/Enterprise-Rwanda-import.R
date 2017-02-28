69a5f545cb2d95ff6d0193d6ca31d6d05b9e582c
Bastiaan Quast <Bastiaan.Quast@unctad.org>
2017-02-28 10:50:03 +0100

    move ratios to clean file

--- R/Enterprise-Rwanda-import.R
+++ R/Enterprise-Rwanda-import.R
@@ -11,28 +11,10 @@ library(dplyr)
 Rwanda_2006 <- read_dta(file = "data/Enterprise/Rwanda/Rwanda2006_manufacturing_clean.dta")
 Rwanda_2011 <- read_dta(file = "data/Enterprise/Rwanda/Rwanda-2011-full data-.dta")
 
-# eliminate negatives
-# l1
-# l5
-# j2b2
-# j2a2
-# j2b1
-# j2a1
-
-# compute ratios 2011
-Rwanda_2011$female_share <- Rwanda_2011$l5 / Rwanda_2011$l1
-
-# computer ratios 2006
-Rwanda_2006$female_share_nonprod <- with(Rwanda_2006, j2b2 / j2a2)
-Rwanda_2006$female_share_prod <- with(Rwanda_2006, j2b1 / j2a1)
-
-# simplify industry
-Rwanda_2011$a4c <- ifelse(Rwanda_2011$a4b < 20, 'Agriculture', ifelse(Rwanda_2011$a4b < 40, 'Manufacturing', 'Services') )
-
 # save
 save(Rwanda_2006,
      Rwanda_2011,
-     file = "data/Enterprise/Rwanda/Rwanda-Enterprise.RData")
+     file = "data/Enterprise/Rwanda/Rwanda-Enterprise-Imported.RData")
 
 # load explore library
 library(explore)
