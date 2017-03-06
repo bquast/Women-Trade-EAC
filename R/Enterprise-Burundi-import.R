aea71404ae18da8b9c26b86d5070ff23d35d4dc8
Bastiaan Quast <bquast@gmail.com>
2017-03-06 10:18:30 +0100

    add importing of WITS tariffs from CSV

--- R/Enterprise-Burundi-import.R
+++ R/Enterprise-Burundi-import.R
@@ -5,13 +5,19 @@
 
 # load library
 library(haven)
+library(readr)
 
 # import Burundi enterprise
 Burundi_2006 <- read_dta(file = "data/Enterprise/Burundi/Burundi2006_manufacturing_clean.dta")
 Burundi_2014 <- read_dta(file = "data/Enterprise/Burundi/Burundi-2014-full data.dta")
 
+
+Burundi_WITS <- read_delim("~/Women-Trade-EAC/data/WITS/Burundi.csv", 
+                           "\t", escape_double = FALSE, trim_ws = TRUE)
+
 # save
 save(Burundi_2006,
      Burundi_2014,
+     Burundi_WITS,
      file = "data/Enterprise/Burundi/Burundi-Enterprise-Imported.RData")
 
