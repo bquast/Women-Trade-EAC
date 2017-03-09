30c7e0151973639c1f51e6f42dcb907ca2b8a935
Bastiaan Quast <bquast@gmail.com>
2017-03-09 12:35:44 +0100

    add female ownership to female owner

--- R/Enterprise-Tanzania-model.R
+++ R/Enterprise-Tanzania-model.R
@@ -26,7 +26,7 @@ t06_np <- formula(female_share_nonprod ~ international + capital_city + business
 t13_pr <- formula(female_share_prod    ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + industry)
 t13_np <- formula(female_share_nonprod ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + industry)
 t13_fo <- formula(female_owner ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + industry)
-t13_fo <- formula(female_owner ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + industry)
+t13_fs <- formula(female_ownership ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + industry)
 
 t13_pr_t <- formula(female_share_prod    ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + Tariff)
 t13_np_t <- formula(female_share_nonprod ~ international + capital_city + business_city + multi_establ + intern_certif + eac_exporter + Tariff)
@@ -44,8 +44,8 @@ t13_np_r1 <-  lm(t13_np, data = Tanzania_2013)
 t13_np_r2 <- glm(t13_np, data = Tanzania_2013, family = quasibinomial(link='logit') )
 t13_fo_r1 <-  lm(t13_fo, data = Tanzania_2013)
 t13_fo_r2 <- glm(t13_fo, data = Tanzania_2013, family = quasibinomial(link='logit') )
-t13_fo_r1 <-  lm(t13_fo, data = Tanzania_2013)
-t13_fo_r2 <- glm(t13_fo, data = Tanzania_2013, family = quasibinomial(link='logit') )
+t13_fs_r1 <-  lm(t13_fs, data = Tanzania_2013)
+t13_fs_r2 <- glm(t13_fs, data = Tanzania_2013, family = quasibinomial(link='logit') )
 
 t13_pr_t_r1 <-  lm(t13_pr_t, data = Tanzania_2013t)
 t13_pr_t_r2 <- glm(t13_pr_t, data = Tanzania_2013t, family = quasibinomial(link='logit') )
@@ -53,22 +53,22 @@ t13_np_t_r1 <-  lm(t13_np_t, data = Tanzania_2013t)
 t13_np_t_r2 <- glm(t13_np_t, data = Tanzania_2013t, family = quasibinomial(link='logit') )
 
 # view output
-summary(t06_pr_r1)
-summary(t06_pr_r2)
+summary(t06_pr_r1) # capital multi_est
+summary(t06_pr_r2) # capital multi_est
 summary(t06_np_r1)
 summary(t06_np_r2)
 
-summary(t13_pr_r1)
-summary(t13_pr_r2)
-summary(t13_np_r1) # good
-summary(t13_np_r2) # good
+summary(t13_pr_r1) # -manufacturing
+summary(t13_pr_r2) # -manufacturing
+summary(t13_np_r1) # international
+summary(t13_np_r2) # international
 
-summary(t13_fo_r1)
-summary(t13_fo_r2)
-summary(t13_fo_r1) # good
-summary(t13_fo_r2) # good
+summary(t13_pr_t_r1) # tariff
+summary(t13_pr_t_r2) # tariff
+summary(t13_np_t_r1) # international
+summary(t13_np_t_r2) # international
 
-summary(t13_pr_t_r1)
-summary(t13_pr_t_r2)
-summary(t13_np_t_r1) # good
-summary(t13_np_t_r2) # good
+summary(t13_fo_r1) # international business manufacturing
+summary(t13_fo_r2) # international business manufacturing
+summary(t13_fs_r1) # intern_certif manufacturing
+summary(t13_fs_r2) # intern_certif manufacturing
